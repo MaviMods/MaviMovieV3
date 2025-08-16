5e73ef797b2e6f48aa1671fe66220f8cfa5c9df4
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2025-08-16 18:15:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,7 +69,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/cinemabotx')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/MODSMAVI')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/mavimods2')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'mavibot_support') # Support Chat Link Without https:// or @
 
@@ -159,4 +159,5 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 
 #MaviMods
+
 
